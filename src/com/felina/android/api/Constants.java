0b41abf1bd982ea1b95a6b5f34868d666eb341f9
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-03-22 23:02:00 +0000

    async client

---
+++ src/com/felina/android/api/Constants.java
@@ -0,0 +1,8 @@
+package com.felina.android.api;
+
+public final class Constants {
+	public static class Params {
+		public static String LOGIN_EMAIL = "email";
+		public static String LOGIN_PASS = "pass";
+	}
+}
--- src/com/felina/android/api/FelinaClient.java
+++ src/com/felina/android/api/FelinaClient.java
@@ -1,119 +1,44 @@
 package com.felina.android.api;
 
-import java.io.IOException;
+import android.content.Context;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.protocol.ClientContext;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.protocol.BasicHttpContext;
-
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.ApacheClient;
-import retrofit.client.Client;
-import retrofit.client.Response;
-import retrofit.http.Field;
-import retrofit.http.FormUrlEncoded;
-import retrofit.http.GET;
-import retrofit.http.POST;
-import android.util.Log;
+import com.felina.android.api.Constants.Params;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+import com.loopj.android.http.RequestParams;
 
 public class FelinaClient {
 	
 	private static final String API_URL = "http://ec2-54-194-186-121.eu-west-1.compute.amazonaws.com";
-	private Felina felina;
+	private static AsyncHttpClient mClient = new AsyncHttpClient();
 	
-	static class LoginCheck {
-		Boolean res;
-		Err err;
+	public FelinaClient(Context context){
+		PersistentCookieStore mCookieStore = new PersistentCookieStore(context);
+		mClient.setCookieStore(mCookieStore);
 	}
 	
-	static class Err {
-		int code;
-		String msg;
+	public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+		mClient.get(getAbsoluteUrl(url), params, responseHandler);
 	}
-	
-	class User {
-		int id;
-		String name;
-		String email;
-		int privilege;
-		String profile_image;
-	}
-	
-	static class Login {
-		Boolean res;
-		Err err;
-		User user;
-	}
-	
-	interface Felina {
-		@GET("/logincheck")
-		LoginCheck loginCheck();
-		
-		@FormUrlEncoded
-		@POST("/login")
-		void login(@Field("email") String email, @Field("pass") String pass, Callback<Login> callback);
-	}
-	
-	public FelinaClient() {
-		
-		Client client = new ApacheClient() {
-		    final CookieStore cookieStore = new BasicCookieStore();
-		    @Override
-		    protected HttpResponse execute(HttpClient client, HttpUriRequest request) throws IOException {
-		        // BasicHttpContext is not thread safe 
-		        // CookieStore is thread safe
-		        BasicHttpContext httpContext = new BasicHttpContext();
-		        httpContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
-		        return client.execute(request, httpContext);
-		    }
-		};
-		
-		RestAdapter restAdapter = new RestAdapter.Builder()
-			.setEndpoint(API_URL)
-			.setClient(client)
-			.build();
-		
-		felina = restAdapter.create(Felina.class);
-	}
-	
-	public Felina getFelina() {
-		return felina;
-	}
-	
-	public Boolean logincheck() {
-		LoginCheck logincheck = felina.loginCheck();
-		Log.d("FelinaClient", "LoginCheck "+logincheck.res+" "+logincheck.err.msg);
-		return logincheck.res;
-	}
-	
-	public Boolean login(String email, String pass) {
-		felina.login(email, pass, new Callback<Login>(){
 
-			@Override
-			public void failure(RetrofitError error) {
-				Log.d("FelinaClient", "Error, body: " + error.getBody().toString());
-			}
+	public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+		mClient.post(getAbsoluteUrl(url), params, responseHandler);
+	}
 
-			@Override
-			public void success(Login login, Response response) {
-				Log.d("FelinaClientL", login.res+" login");
-				new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						// TODO Auto-generated method stub
-						logincheck();
-					}
-				}).start();
-			}
-		});
-		return true;
+	private static String getAbsoluteUrl(String relativeUrl) {
+		return API_URL + relativeUrl;
+	}
+	
+	public void login(String email, String pass, AsyncHttpResponseHandler responseHandler) {
+		RequestParams params = new RequestParams();
+		params.put(Params.LOGIN_EMAIL, email);
+		params.put(Params.LOGIN_PASS, pass);
+		post("/login", params, responseHandler);
+	}
+	
+	public void logincheck(AsyncHttpResponseHandler responseHandler) {
+		get("/logincheck", null, responseHandler);
 	}
 
 }
