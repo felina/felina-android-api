3442ba11e98515f561fa86962d69e56d86f9d7fd
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-03-23 19:16:20 +0000

    image upload

--- src/com/felina/android/api/Constants.java
+++ src/com/felina/android/api/Constants.java
@@ -4,5 +4,7 @@ public final class Constants {
 	public static class Params {
 		public static String LOGIN_EMAIL = "email";
 		public static String LOGIN_PASS = "pass";
+		public static String IMG_FILE = "imgUpload";
+		public static String IMG_PROJECT = IMG_FILE+"_projectid";
 	}
 }
--- src/com/felina/android/api/FelinaClient.java
+++ src/com/felina/android/api/FelinaClient.java
@@ -1,5 +1,11 @@
 package com.felina.android.api;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.apache.http.Header;
+import org.apache.http.message.BasicHeader;
+
 import android.content.Context;
 
 import com.felina.android.api.Constants.Params;
@@ -13,8 +19,10 @@ public class FelinaClient {
 //	private static final String API_URL = "http://ec2-54-194-186-121.eu-west-1.compute.amazonaws.com";
 	private static final String API_URL = "http://nl.ks07.co.uk:5005";
 	private static AsyncHttpClient mClient = new AsyncHttpClient();
+	private static Context context;
 	
-	public FelinaClient(Context context){
+	public FelinaClient(Context c){
+		context = c;
 		PersistentCookieStore mCookieStore = new PersistentCookieStore(context);
 		mClient.setCookieStore(mCookieStore);
 	}
@@ -46,6 +54,28 @@ public class FelinaClient {
 		RequestParams params = new RequestParams();
 		params.put(Params.LOGIN_EMAIL, email);
 		get("/token", params, responseHandler);
+	}
+	
+	public void postImg(File f, String contentType, AsyncHttpResponseHandler responseHandler) {
+		RequestParams params = new RequestParams();
+		try {
+			params.put(Params.IMG_FILE, f, contentType);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		post("/img", params, responseHandler);
+	}
+	
+	public void postImg(File f, String contentType, int id, AsyncHttpResponseHandler responseHandler) {
+		RequestParams params = new RequestParams();
+		try {
+			params.put(Params.IMG_FILE, f, contentType);
+			params.put(Params.IMG_PROJECT, id);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		post("/img", params, responseHandler);
+		
 	}
 
 }
